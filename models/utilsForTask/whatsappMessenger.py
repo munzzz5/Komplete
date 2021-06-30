63e2e7ff9031087c20ea2e85189d3c63252f7faf
Akshay Bhandari <akshay.b1@outlook.com>
2021-06-30 18:17:24 +0530

    removed sensitive info

--- models/utilsForTask/whatsappMessenger.py
+++ models/utilsForTask/whatsappMessenger.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
-account_sid = 'AC57b5b2abed9c0e4b2f31daef947dfecf'
-auth_token = 'a78f3259eaca573c752940b630978957'
+account_sid = 'ACC SID'
+auth_token = 'AUTH TOKEN'
 client = Client(account_sid, auth_token)
-api_key = 'SK5d8913a0fe198725de565d6caac74273'
+api_key = 'APIKEY'
 message = client.messages.create(
     from_='whatsapp:+13124677853',
     body='Your appointment is coming up on July 21 at 3PM',
